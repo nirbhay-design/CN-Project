bee4f61ab19fa13d91dfac727a3ce97601e3e3dc
sharma.59 <sharma.59@iitjac.in>
2022-04-16 15:11:12 +0530

    commiting base project

--- public/script.js
+++ public/script.js
@@ -1,8 +1,17 @@
+const socket = io();
 const videogrid = document.getElementById('video-grid')
 const myvideo = document.createElement("video");
-myvideo.muted = false;
+// myvideo.muted = true;
+myvideo.autoplay = true;
 
-console.log(videogrid)
+// console.log(videogrid)
+var peer = new Peer(undefined, {
+    path:'/peerjs',
+    host:'/',
+    port:'3000',
+});
+
+
 
 let myvideostream;
 navigator.mediaDevices.getUserMedia({
@@ -11,7 +20,37 @@ navigator.mediaDevices.getUserMedia({
 }).then((stream) => {
     myvideostream = stream;
     addvideostream(myvideo,stream)
+    peer.on('call',call => {
+        call.answer(stream)
+        const video = document.createElement('video')
+        // video.muted = true;
+        // video.autoplay = true;
+        call.on('stream',userVideoStream => {
+            addvideostream(video,userVideoStream);
+        })
+    })
+    socket.on('user-connected',(userId)=>{
+        setTimeout(()=>{
+            connectToNewUser(userId, stream);
+        },1000)
+    })
 })
+
+peer.on('open',id => {
+    socket.emit('join-room',ROOM_ID,id);
+})
+
+
+const connectToNewUser = (userId, stream) => {
+    const call = peer.call(userId,stream)
+    const video = document.createElement('video')
+    // video.muted = true;
+    video.autoplay = true;
+    call.on('stream',userVideoStream => {
+        addvideostream(video,userVideoStream);
+    })
+
+}
 
 const addvideostream = (video,stream) => {
     video.srcObject = stream;
@@ -20,5 +59,84 @@ const addvideostream = (video,stream) => {
     })
     videogrid.append(video)
 }
+
+let text = $('input')
+console.log(text.val())
+
+$('input').keydown((e) =>{
+    if (e.which == 13 && text.val().length !== 0) {
+        socket.emit('message',text.val())
+        // console.log(text.val())
+        text.val('')
+    }
+})
+
+socket.on('createMessage',message=>{
+    // console.log('this is coming from server',message)
+    let date = new Date()
+    let hour = date.getHours()
+    let mins = date.getMinutes()
+    $('.messages').append(`<span class="message"><b style="font-weight:bold;">User: ${(hour > 12)?hour-12:hour}:${mins} ${(hour >= 12)?"PM":"AM"} </b> <br/>${message}</span>`)
+})
+
+
+const muteUnmute = () =>{
+    const enabled = myvideostream.getAudioTracks()[0].enabled;
+    if (enabled) {
+        myvideostream.getAudioTracks()[0].enabled = false;
+        setUnmuteButton();
+    } else{
+        setMuteButton();
+        myvideostream.getAudioTracks()[0].enabled = true;
+    }
+}
+
+const setUnmuteButton = () =>{
+    const myhtml = `
+        <i class="unmute fa-solid fa-microphone-slash"></i>
+        <span>UnMute</span>
+    `
+
+    $('.main__mute__button').html(myhtml)    
+}
+
+const setMuteButton = () =>{
+    const myhtml = `
+        <i class="fa fa-microphone"></i>
+        <span>Mute</span>
+    `
+
+    $('.main__mute__button').html(myhtml)
+}
+
+const playStop = () =>{
+    const enabled = myvideostream.getVideoTracks()[0].enabled;
+    if (enabled) {
+        myvideostream.getVideoTracks()[0].enabled = false;
+        setPlayVideo();
+    } else{
+        setStopVideo();
+        myvideostream.getVideoTracks()[0].enabled = true;
+    }
+}
+
+const setPlayVideo = () =>{
+    const myhtml = `
+        <i class="unvideo fa-solid fa-video-slash"></i>
+        <span>UnVideo</span>
+    `
+
+    $('.main__video__button').html(myhtml)    
+}
+
+const setStopVideo = () =>{
+    const myhtml = `
+    <i class="fa fa-video-camera"></i>
+    <span>Video</span>
+    `
+
+    $('.main__video__button').html(myhtml)
+}
+
 
 
--- server.js
+++ server.js
@@ -2,10 +2,17 @@ const express = require('express');
 const {v4:uuidv4} = require('uuid')
 const app = express();
 const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server,{
+    debug:true
+})
 
 app.set('view engine','ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended:true}))
+app.use('/peerjs',peerServer)
 
 app.get('/',(req,res)=>{
     res.redirect(`/${uuidv4()}`)
@@ -15,7 +22,23 @@ app.get('/:room',(req,res)=>{
     res.render('room',{roomId: req.params.room})
 })
 
-const Port = process.env.PORT || 3000
-app.listen(Port,()=>{
-    console.log(`app is listening at ${Port} port`)
+io.on('connection',socket=>{
+    socket.on('join-room', (roomId, userId) => {
+        socket.join(roomId)
+        socket.broadcast.to(roomId).emit('user-connected', userId)
+        socket.on('message',message => {
+            io.to(roomId).emit('createMessage',message)
+        })
+    })
 })
+
+const Port = process.env.PORT || 3000
+// app.listen(Port,()=>{
+//     console.log(`app is listening at ${Port} port`)
+// })
+
+server.listen(Port,()=>{
+    console.log(`app is listening at port ${Port}`)
+})
+
+// 1:22:15 / 3:28:02
